36b59901c8fdd94f8a75095d95f1db56ebc378db
LG972021 <liamgill97@hotmail.com>
2023-02-03 23:29:55 +0000

    Added Skeleton Graph Functionality

---
+++ src/Coponents/Dropdown.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './Dropdown.css'
+
+const Dropdown = ({ open, menu, optionOnClick, labelText }) => {
+
+    const [dropDownOpen, setDropDownOpen] = useState(false);
+
+
+    return (
+      <div className="dropdown">
+        <p onClick={()=>{setDropDownOpen(!dropDownOpen)}} style={{width: '152px', height: '25px' ,textAlign: 'center', color: 'white', marginTop: '3px', marginBottom: '3px'}}>{labelText}</p>
+        {dropDownOpen ? (
+          <ul className="menu">
+            {menu.map((menuItem, index) => (
+              <li onClick={()=>{
+                optionOnClick(menuItem);
+                setDropDownOpen(false);
+            }}  key={index} className="menu-item">{menuItem}</li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    );
+  };
+
+  export default Dropdown;
--- src/Coponents/Home.jsx
+++ src/Coponents/Home.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
+import useWindowDimensions from '../Utils/useWindowDimensions';
+import Dropdown from './Dropdown';
+
 function Home () {
+    const { height, width } = useWindowDimensions();
+
+    const [xAxisLabel, setXAxisLabel] = useState("X-Axis");
+    const [yAxisLabel, setYAxisLabel] = useState("Y-Axis");
+
     return (
-    <div style={{width: '100%', height: 'screen', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-   
-        <Plot
-            data={[
-            {
-                x: [1, 2, 3],
-                y: [2, 6, 3],
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: {color: 'red'},
-            },
-            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-            ]}
-            layout={ {width: 640, height: 480, title: 'A Fancy Plot'} }
-        />
+    <div style={{width: '100%', height: height , display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',height: '480px', width: '50%'}}>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',height: '100%', width: '90%', backgroundColor: 'white', borderRadius: '25px'}}>
+                <Dropdown open={true} menu={["A", "B", "C"]} optionOnClick={setXAxisLabel} labelText={"X Axis"}/>
+                <Dropdown open={true} menu={["one", "two", "three"]} optionOnClick={setYAxisLabel} labelText={"Y Axis"}/>
+            </div>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%'}}>
+            <Plot
+                data={[
+                {
+                    x: [1, 2, 3],
+                    y: [2, 6, 3],
+                    type: 'scatter',
+                    mode: 'markers',
+                    marker: {color: 'teal'},
+                },
+                // {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                ]}
+                layout={ {width: 640, height: 480, 
+                    // title: 'A Fancy Plot',
+                xaxis: {
+                    title: {
+                      text: xAxisLabel,
+                      font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                      }
+                    },
+                  },
+                  yaxis: {
+                    title: {
+                      text: yAxisLabel,
+                      font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                      }
+                    }
+                  }} }            
+            />
+        </div>
     
     </div>
     );
